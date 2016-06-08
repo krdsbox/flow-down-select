7f04a4826db75b085740c09aee9bf8f987dfe2de
Logesh Kumar <logesh.kumar@krds.fr>
2016-06-08 17:19:14 +0530

    initial flow down select

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-// your js here
-window.console && console.log("Welcome to box starter");
+$(document).ready(function() {
+
+	$(".selLabel").on('mousedown', function () {
+		$('.dropdown').toggleClass('active');
+	});
+
+	$(".dropdown-list li").click(function() {
+		$('.selLabel').text($(this).text());
+		$('.dropdown').removeClass('active');
+		$('.selected-item p span').text($('.selLabel').text());
+	});
+
+});
